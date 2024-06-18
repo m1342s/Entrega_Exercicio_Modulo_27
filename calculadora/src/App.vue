db38df46772e7b95c49729ef3ce4e4b062bd3e36
Matheus Viersa <matheusviersa995@hotmail.com>
2024-06-18 14:38:47 -0300

    Código a ser revisto

---
+++ calculadora/src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { reactive } from "vue";
+import Formulario from "./components/Formulario.vue";
+
+const estado = reactive({
+  filtro: "multiplicacao",
+  n1: 0,
+  n2: 0,
+});
+
+const multiplicacao = ({n1, n2}=estado) => {
+  return n1 * n2;
+};
+const adicao =  ({n1, n2}=estado) => {
+  return n1 + n2;
+};
+const subtracao =  ({n1, n2}=estado) => {
+  return n1 - n2;
+};
+const divisao =  ({n1, n2}=estado) => {
+  return n1 / n2;
+};
+
+
+
+const calculadora = () => {
+  const { n1, n2, filtro } = estado;
+
+  if ((n1, n2)) {
+    switch (filtro) {
+      case "divisao":
+        return divisao();
+      case "multiplicacao":
+        return multiplicacao();
+      case "adicao":
+        return adicao();
+      case "subtracao":
+        return subtracao();
+    }
+  }
+};
+</script>
+
+<template>
+  <h1 class="d-flex justify-content-center mt-3">Calculadora aritmética</h1>
+  <Formulario
+    :trocaFiltro="(event) => (estado.filtro = event.target.value)"
+    :valorN1="(event) => (estado.n1 =Number(event.target.value))"
+    :valorN2="(event) => (estado.n2 =Number(event.target.value))"
+  />
+  <div class="d-block pb-3">
+  Resultado:<span>{{ calculadora() }}</span>
+  
+  </div>
+</template>
+
+<style scoped></style>
---
+++ calculadora/src/components/Formulario.vue
@@ -0,0 +1,39 @@
+<script setup>
+const props=defineProps(["valorN1","valorN2","trocaFiltro","calculadora"])
+</script>
+
+
+<template>
+  <form class="ms-4 d-flex flex-column justify-content-center ">
+    <div class="d-flex flex-column mx-auto">
+      <div class="d-block pb-3">
+        <label for="numero1">Número 1</label>
+        <input @keyup="props.valorN1"
+          style="width: 150px"
+          type="number"
+          step="0.01"
+          class="form-control"
+          id="numero1"
+        />
+      </div>
+      <div class="d-block pb-3">
+        <label for="numero2">Número 2</label>
+        <input
+        @keyup="props.valorN2"
+          style="width: 150px"
+          type="number"
+          step="0.01"
+          id="numero2"
+          class="form-control"
+        />
+      </div>
+     
+      <select @change="props.trocaFiltro" class="form-control" name="" id="select">
+        <option value="multiplicaçao">Multiplicação</option>
+        <option value="adicao">Adição</option>
+        <option value="subtracao">Subtração</option>
+        <option value="divisao">Divisão</option>
+      </select>
+    </div>
+  </form>
+</template>
